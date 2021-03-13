b854853cc59b8ef835e16cdeeb2608d1f41cc741
Ernest Guevarra <ernest@guevarra.io>
2021-03-13 18:21:24 +0000

    initial commit

---
+++ R/radafruitio.R
@@ -0,0 +1,22 @@
+################################################################################
+#
+#'
+#' An Interface to the Adafruit IO API
+#'
+#' [**Adafruit IO**](https://io.adafruit.com) is a platform designed by
+#' [Adafruit](https://www.adafruit.com) to display, respond, and interact with
+#' any internet of things (IoT) project's data. The
+#' [**Adafruit IO**](https://io.adafruit.com) HTTP API provides access to
+#' [**Adafruit IO**](https://io.adafruit.com) data from any programming language
+#' or hardware environment that can speak HTTP. The `radafruitio` package
+#' provides functions to interface with the
+#' [**Adafruit IO**](https://io.adafruit.com) HTTP API through R.
+#'
+#' @docType package
+#' @keywords internal
+#' @name radafruitio
+#'
+#
+################################################################################
+"_PACKAGE"
+
---
+++ README.md
@@ -0,0 +1,10 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# radafruitio: An Interface to the Adafruit IO API
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
